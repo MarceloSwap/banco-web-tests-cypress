883bfe675f2231e99aa523422da9dc342fa44e16
MarceloSwap <126257691+MarceloSwap@users.noreply.github.com>
2026-04-02 13:21:11 -0300

    test: adiciona teste de login com credenciais inválidas

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -3,7 +3,47 @@ describe('Login', () => {
     cy.visit('http://localhost:4000');
     cy.get('#username').click().type('marcelo.ferreira');
     cy.get('#senha').click().type('123456');
-    cy.get('#login-section > .btn').click();
+    cy.contains('button', 'Entrar').click();
     cy.contains('h4', 'Realizar Transferência').should('be.visible');
-  })
+  });
+
+  it('Login com dados inválidos devem apresentar mensagem de erro', () => {
+    // Arrange
+    cy.visit('http://localhost:4000');
+
+    // Act
+    cy.get('#username').click().type('marcelo.nietzsche');
+    cy.get('#senha').click().type('x1x2x3X4@'); // semha inválida
+    cy.contains('button', 'Entrar').click();
+
+    // Assert
+    cy.get('.toast').should('have.text', 'Erro no login. Tente novamente.')
+  });
+
+  it('Login com username inválido deve apresentar mensagem de erro', () => {
+    // Arrange
+    cy.visit('http://localhost:4000');
+
+    // Act
+    cy.get('#username').click().type('marcelo.nietzsche');
+    cy.get('#senha').click().type('123456'); // semha inválida
+    cy.contains('button', 'Entrar').click();
+    // Assert
+    cy.get('.toast').should('have.text', 'Erro no login. Tente novamente.')
+  });
+
+  it('Login com senha inválida deve apresentar mensagem de erro', () => {
+    // Arrange
+    cy.visit('http://localhost:4000');
+
+    // Act
+    cy.get('#username').click().type('marcelo.ferreira');
+    cy.get('#senha').click().type('654123'); // semha inválida
+    cy.contains('button', 'Entrar').click();
+
+    // Assert
+    cy.get('.toast').should('have.text', 'Erro no login. Tente novamente.')
+  });
+
+
 })
